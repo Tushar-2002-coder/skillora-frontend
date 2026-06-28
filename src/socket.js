2f6dd0598b421d04de78eb0fc6affd6400708c7b
Tushar-2002-coder <tusharsharma89372@gmail.com>
2026-06-28 20:07:28 +0530

    initial commit

--- src/socket.js
+++ src/socket.js
@@ -5,6 +5,7 @@ const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
 
 const socket = io(SOCKET_URL, {
   withCredentials: true, // Yeh zaroori hai kyunki tumne server par credentials: true rakha hai
+  transports: ['websocket', 'polling'], // Pehle WebSocket try karega, phir Polling
 });
 
 window.socket = socket; 
